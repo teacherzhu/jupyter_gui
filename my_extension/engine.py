fa9cfb57e8c5beadc7c4e0c7795681d4b9e8e5af
KwatME <kwat.medetgul.ernar@gmail.com>
2016-11-15 12:31:43 -0800

    Need to send var to global scope

--- my_extension/engine.py
+++ my_extension/engine.py
@@ -25,7 +25,6 @@ def simplex(path_to_include, library_name, function_name, req_args, opt_args, re
     returned = locals()['function'](**args)
 
     for n, r in zip(return_names, returned):
-        print('updating ', n, r)
         globals()[n] = r
 
 
@@ -53,7 +52,7 @@ def process_args(req_args, opt_args):
                 processed = v
 
             elif ',' in v:  # Assume iterable
-                processed = [cast_string_to_int_float_bool_or_str(s) for s in v.split(',')]
+                processed = [cast_string_to_int_float_bool_or_str(s) for s in v.split(',') if s]
                 print('Argument: \'{}\' ==> {} ...'.format(v, processed))
 
             else:  # Using as it is (str)
