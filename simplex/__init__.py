55345c47d4731a65b26fb45938aca6f8a2832731
KwatME <kwat.medetgul.ernar@gmail.com>
2016-11-30 17:46:28 -0800

    Ready for testing

--- simplex/__init__.py
+++ simplex/__init__.py
@@ -1,150 +1,21 @@
-from os import environ, listdir
-from os.path import join, split, isfile, isdir
-import json
+from os import environ
+from os.path import join
 
+from .support import load_libraries, make_task_json
 from .taskmanager import TaskManager
-
 
 # ======================================================================================================================
 # Set up environment
 # ======================================================================================================================
-def load_config(filepath):
-    """
-
-    :param filepath: str; config.txt
-    :return:
-    """
-
-    config = {}
-    with open(filepath) as f:
-        for line in f:
-            k, v = line.strip().split('=')
-            config[k] = v
-
-    print('Simplex configuration:')
-    for k, v in sorted(config.items()):
-        print('\t{} : {}'.format(k, v))
-
-    return config
-
-
-def load_libraries(directory_path):
-    """
-
-    :param directory_path: str; simplex_data directory
-    :return: list; list of library directory paths
-    """
-
-    libs = []
-    for f in listdir(directory_path):
-        fp = join(directory_path, f)
-        if isdir(fp):
-            libs.append(fp)
-    libs = sorted(libs)
-
-    print('Simple libraries:')
-    for lib in libs:
-        print('\t{}'.format(lib))
-
-    return libs
-
-
-def check_jsons(directory_paths):
-    """
-
-    :param directory_paths: list; list of library directory paths
-    :return: None
-    """
-
-    for dp in directory_paths:
-        lib = split(dp)[1]
-
-        fp_json = join(dp, '{}.json'.format(lib))
-        try:
-            load_json(fp_json)
-        except FileNotFoundError:
-            raise FileNotFoundError('{} library is missing {}.json.'.format(dp, lib))
-        except KeyError:
-            raise ValueError('Error loading {}.'.format(fp_json))
-    else:
-        print('All .json files are good.')
-
-
-def load_json(filepath):
-    """
-
-    :param filepath: str; full path to library.json
-    :return: None
-    """
-
-    if isfile(filepath):
-        raise FileNotFoundError('The file {} isn\'t found or isn\'t an absolute path.')
-
-    # Open .json
-    with open(filepath) as f:
-        library = json.load(f)
-
-    processed_tasks = {}
-
-    # Library name
-    library_name = library['library_name']
-
-    # Library path
-    if 'library_path' in library:
-        library_path = library['library_path']
-        if not library_path.endswith('/'):
-            library_path += '/'
-            print('Appended \'/\' to library_path, which is now: {}.'.format(library_path))
-    else:
-        library_path = join(split(filepath)[0], '')
-        print('No library path is specified for {} library so guessed to be {}.'.format(library_name, library_path))
-    if not isdir(library_path):
-        library_path = join(HOME_DIR, library_path)
-        print('Converted the library path {} to the absolute path relative to $HOME directory.'.format(library_path))
-
-    # Tasks
-    tasks = library['tasks']
-    for task in tasks:
-
-        # Task label is this task's UID
-        label = task['label']
-        if label in processed_tasks:
-            raise ValueError('Multiple \'{}\' task labels found! Use unique task label for each task.'.format(label))
-        else:
-            processed_tasks[label] = {}
-
-        # Function name
-        processed_tasks[label]['function_name'] = task['function_name']
-
-        # Description
-        if 'description' in task:
-            processed_tasks[label]['description'] = task['description']
-
-        # Arguments
-        if 'required_args' in task:
-            processed_tasks[label]['required_args'] = task['required_args']
-        if 'optional_args' in task:
-            processed_tasks[label]['optional_args'] = task['optional_args']
-        if 'default_args' in task:
-            processed_tasks[label]['default_args'] = task['default_args']
-
-        # Returns
-        if 'returns' in task:
-            processed_tasks[label]['returns'] = task['returns']
-
-    return processed_tasks
-
-
 HOME_DIR = environ['HOME']
-SIMPLEX_DIR = join(HOME_DIR, 'simplex/')
+SIMPLEX_REPO_DIR = join(HOME_DIR, 'simplex/')
+SIMPLEX_DIR = join(SIMPLEX_REPO_DIR, 'simplex/')
 
-SIMPLEX_CONFIG_FILE = join(SIMPLEX_DIR, 'config.sh')
-SIMPLEX_CONFIG = load_config(SIMPLEX_CONFIG_FILE)
-
-SIMPLEX_DATA_DIR = join(SIMPLEX_DIR, 'simplex_data/')
+SIMPLEX_DATA_DIR = join(SIMPLEX_DIR, 'default_libs/')
 SIMPLEX_LIBRARIES = load_libraries(SIMPLEX_DATA_DIR)
 
-check_jsons(SIMPLEX_LIBRARIES)
+TASK_JSON_FILEPATH = join(SIMPLEX_DIR, 'static', 'resources', 'tasks.json')
+make_task_json(SIMPLEX_LIBRARIES, TASK_JSON_FILEPATH)
 
 
 # ======================================================================================================================
--- simplex/support.py
+++ simplex/support.py
@@ -1,11 +1,14 @@
-import os
+from os import environ, listdir
+from os.path import join, split, isfile, isdir
 import json
+
+HOME_DIR = environ['HOME']
 
 
 def load_config(filepath):
     """
 
-    :param filepath:
+    :param filepath: str; config.txt
     :return:
     """
 
@@ -25,46 +28,62 @@ def load_config(filepath):
 def load_libraries(directory_path):
     """
 
-    :param directory_path:
-    :return:
+    :param directory_path: str; simplex_data directory
+    :return: list; list of library directory paths
     """
 
     libs = []
-    for f in os.listdir(directory_path):
-        fp = os.path.join(directory_path, f)
-        if os.path.isdir(fp):
+    for f in listdir(directory_path):
+        fp = join(directory_path, f)
+        if isdir(fp):
             libs.append(fp)
     libs = sorted(libs)
 
-    print('Simple libraries:')
+    print('SimpleX libraries:')
     for lib in libs:
         print('\t{}'.format(lib))
 
     return libs
 
 
-def check_jsons(directory_paths):
-    for dp in directory_paths:
-        lib = os.path.split(dp)[1]
+def make_task_json(directory_paths, filepath):
+    """
 
-        fp_json = os.path.join(dp, '{}.json'.format(lib))
+    :param directory_paths: list; list of library directory paths
+    :param filepath: str; tasks.json containing all available SimpleX task specifications
+    :return: dict; all available SimpleX task specifications
+    """
+
+    tasks_by_libraries = {}
+    for dp in directory_paths:
+        lib = split(dp)[1]
+
+        fp_json = join(dp, '{}.json'.format(lib))
         try:
-            load_json(fp_json)
+            tasks = load_json(fp_json)
+            tasks_by_libraries.update(tasks)
         except FileNotFoundError:
             raise FileNotFoundError('{} library is missing {}.json.'.format(dp, lib))
         except KeyError:
             raise ValueError('Error loading {}.'.format(fp_json))
-    else:
-        print('All .json files are good.')
+
+    with open(filepath, 'w') as f:
+        json.dump(tasks_by_libraries, f, sort_keys=True, indent=2)
+
+    return tasks_by_libraries
 
 
-def load_json(filepath, directory_path):
+def load_json(filepath):
     """
 
-    :param filepath:
-    :param directory_path:
-    :return:
+    :param filepath: str; full path to library.json
+    :return: None
     """
+
+    print('Loading {} ...'.format(filepath))
+
+    if not isfile(filepath):
+        raise FileNotFoundError('The file {} isn\'t found or isn\'t an absolute path.')
 
     # Open .json
     with open(filepath) as f:
@@ -76,18 +95,19 @@ def load_json(filepath, directory_path):
     library_name = library['library_name']
 
     # Library path
-    if 'library_path' in library:
+    if 'library_path' in library:  # Use specified library path
         library_path = library['library_path']
-    else:
-        library_path = os.path.join(directory_path, library_name, '')
-        print('No library path is specified for {} library so guessed to be {}.'.format(library_name, library_path))
-    if not library_path.endswith('/'):
-        library_path += '/'
-        print('Appended \'/\' to library_path, which is now: {}.'.format(library_path))
-    if not os.path.isdir(library_path):
-        library_path = os.path.join(filepath.split(library_path)[0], library_path)
-        if not os.path.isdir(library_path):
-            raise ValueError('Error converting the library path {} to the absolute path.'.format(library_path))
+        if not library_path.endswith('/'):  # Make sure the library path ends with '/'
+            library_path += '/'
+            print('\tAppended \'/\' to library_path, which is now: {}.'.format(library_path))
+        if not isdir(library_path):  # Use absolute path
+            library_path = join(HOME_DIR, library_path)
+            print('\tConverted the library path to the absolute path relative to the $HOME directory: {}.'.format(
+                library_path))
+
+    else:  # Guess library path
+        library_path = join(split(filepath)[0], '')
+        print('\tNo library path is specified for {} library so guessed to be {}.'.format(library_name, library_path))
 
     # Tasks
     tasks = library['tasks']
@@ -100,23 +120,98 @@ def load_json(filepath, directory_path):
         else:
             processed_tasks[label] = {}
 
+        processed_tasks[label]['library_path'] = library_path
+        processed_tasks[label]['library_name'] = library_name
+
         # Function name
         processed_tasks[label]['function_name'] = task['function_name']
 
         # Description
         if 'description' in task:
             processed_tasks[label]['description'] = task['description']
+        else:
+            processed_tasks[label]['description'] = ''
 
         # Arguments
-        if 'required_args' in task:
-            processed_tasks[label]['required_args'] = task['required_args']
-        if 'optional_args' in task:
-            processed_tasks[label]['optional_args'] = task['optional_args']
-        if 'default_args' in task:
-            processed_tasks[label]['default_args'] = task['default_args']
+        for a in ['required_args', 'optional_args', 'default_args']:
+            if a in task:
+                required_args = task[a]
+                processed_tasks[label][a] = process_args(required_args)
+            else:
+                processed_tasks[label][a] = []
 
         # Returns
         if 'returns' in task:
             processed_tasks[label]['returns'] = task['returns']
 
     return processed_tasks
+
+
+def process_args(args):
+    processed_args = []
+    for a in args:
+        processed_a = dict()
+
+        processed_a['arg_name'] = a['arg_name']
+
+        if 'default_value' in a:
+            processed_a['value'] = a['value']
+        else:
+            processed_a['value'] = ''
+
+        if 'label' in a:
+            processed_a['label'] = a['label']
+        else:
+            processed_a['label'] = a['arg_name']
+
+        if 'description' in a:
+            processed_a['description'] = a['description']
+        else:
+            processed_a['description'] = ''
+
+        processed_args.append(processed_a)
+
+    return processed_args
+
+
+def merge_dicts(*dicts):
+    """
+    Shallow copy and merge dicts into a new dict; precedence goes to
+    key value pairs in latter dict.
+    :param dicts: iterable of dict;
+    :return: dict;
+    """
+
+    merged = {}
+    for d in dicts:
+        merged.update(d)
+
+    return merged
+
+
+def cast_string_to_int_float_bool_or_str(string):
+    """
+    Convert string into the following data types (return the first successful):
+    int, float, bool, or str.
+    :param string: str;
+    :return: int, float, bool, or str;
+    """
+
+    value = string.strip()
+
+    # try to cast to int or float
+    for var_type in [int, float]:
+        try:
+            converted_var = var_type(value)
+            return converted_var
+        except ValueError:
+            pass
+
+    # try to cast as boolean
+    if value == 'True':
+        return True
+    elif value == 'False':
+        return False
+
+    # return as string last priority
+    return str(value)
