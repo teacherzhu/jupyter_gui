1c6049f62515f4b586020a8bed819c4bb19d56c0
KwatME <kwat.medetgul.ernar@gmail.com>
2017-02-08 17:42:36 -0800

    Works

--- simpli/startup.py
+++ simpli/startup.py
@@ -1,94 +1,84 @@
-from IPython import get_ipython  # For syntax (reimported when a Notebook starts by default)
-
-global mgr
+from IPython import get_ipython  # For syntax (re-imported by default when a Notebook starts)
 
 
-def init_libs():
-    """
-
-    :return:
-    """
-
-    global json
-    import json
-
-    from simpli import Manager
-
-    # Initialize a Manager
-    global mgr
-    mgr = Manager()
-
-
-def load_web_components():
-    """
-
-    :return:
-    """
-
-    global dwidgets
-    import declarativewidgets as dwidgets
-
-    # Initialize declarative widgets
-    dwidgets.init()
-
-    imports = '''
-    <link rel='import' href='urth_components/iron-form/iron-form.html'
-          is='urth-core-import' package='PolymerElements/iron-form'>
-    <link rel='import' href='urth_components/paper-input/paper-input.html'
-          is='urth-core-import' package='PolymerElements/paper-input'>
-    <link rel='import' href='urth_components/iron-label/iron-label.html'
-          is='urth-core-import' package='PolymerElements/iron-label'>
-    <link rel='import' href='urth_components/paper-button/paper-button.html'
-          is='urth-core-import' package='PolymerElements/paper-button'>
-    <link rel='import' href='urth_components/iron-icon/iron-icon.html'
-          is='urth-core-import' package='PolymerElements/iron-icon'>
-    <link rel='import' href='urth_components/paper-material/paper-material.html'
-          is='urth-core-import' package='PolymerElements/paper-material'>
-    <link rel='import' href='urth_components/paper-header-panel/paper-header-panel.html'
-          is='urth-core-import' package='PolymerElements/paper-header-panel'>
-    <link rel='import' href='urth_components/iron-collapse/iron-collapse.html'
-          is='urth-core-import' package='PolymerElements/iron-collapse'>
-    <link rel='import' href='urth_components/paper-collapse-item/paper-collapse-item.html'
-          is='urth-core-import' package='Collaborne/paper-collapse-item'>
-    '''
-
-    get_ipython().run_cell_magic('HTML', '', imports)
-
-
+# ======================================================================================================================
+# Define functions
+# ======================================================================================================================
 def sync_manager_to_notebook():
     """
-
-    :return:
+    Sync namespace: Manager ==> Notebook
+    :return: None
     """
 
-    # Manager ==> Notebook
     for n, v in mgr.namespace.items():
         globals()[n] = v
 
 
 def sync_notebook_to_manager():
     """
-
+    Sync namespace: Notebook ==> Manager
     :return:
     """
 
-    # Notebook ==> Manager
     mgr.update_namespace(globals())
 
 
-# Register kernel initialization callback
-if (init_libs not in get_ipython().events.callbacks['shell_initialized']):
-    get_ipython().events.register('shell_initialized', init_libs)
+def load_gui():
+    """
+    Load GUI components.
+    :return: None
+    """
 
-if (load_web_components not in get_ipython().events.callbacks['shell_initialized']):
-    get_ipython().events.register('shell_initialized', load_web_components)
+    global dwidgets
+    import declarativewidgets as dwidgets
 
+    dwidgets.init()
+
+    imports = '''
+        <link rel='import' href='urth_components/iron-form/iron-form.html'
+              is='urth-core-import' package='PolymerElements/iron-form'>
+        <link rel='import' href='urth_components/paper-input/paper-input.html'
+              is='urth-core-import' package='PolymerElements/paper-input'>
+        <link rel='import' href='urth_components/iron-label/iron-label.html'
+              is='urth-core-import' package='PolymerElements/iron-label'>
+        <link rel='import' href='urth_components/paper-button/paper-button.html'
+              is='urth-core-import' package='PolymerElements/paper-button'>
+        <link rel='import' href='urth_components/iron-icon/iron-icon.html'
+              is='urth-core-import' package='PolymerElements/iron-icon'>
+        <link rel='import' href='urth_components/paper-material/paper-material.html'
+              is='urth-core-import' package='PolymerElements/paper-material'>
+        <link rel='import' href='urth_components/paper-header-panel/paper-header-panel.html'
+              is='urth-core-import' package='PolymerElements/paper-header-panel'>
+        <link rel='import' href='urth_components/iron-collapse/iron-collapse.html'
+              is='urth-core-import' package='PolymerElements/iron-collapse'>
+        <link rel='import' href='urth_components/paper-collapse-item/paper-collapse-item.html'
+              is='urth-core-import' package='Collaborne/paper-collapse-item'>
+        '''
+
+    get_ipython().run_cell_magic('HTML', '', imports)
+
+
+# ======================================================================================================================
+# Register
+# ======================================================================================================================
 # Register post execute cell callback
 if sync_notebook_to_manager not in get_ipython().events.callbacks['post_execute']:
     get_ipython().events.register('post_execute', sync_notebook_to_manager)
 
-# Initial namespace sync
-init_libs()
-load_web_components()
+# ======================================================================================================================
+# Start up
+# ======================================================================================================================
 # TODO: remove?
+global json
+import json
+
+# Initialize a Manager.
+from simpli import Manager
+
+# TODO: rename to 'manager'
+global mgr
+mgr = Manager()
+# TODO: remove (nothing to sync in the beginning)?
 sync_notebook_to_manager()
+
+load_gui()
