9332c6b42e15eb4fdc0a0e82a79ea2ffebb4f5e9
KwatME <kwat.medetgul.ernar@gmail.com>
2017-02-18 20:10:37 -0800

    Added progress abr

--- simpli/startup.py
+++ simpli/startup.py
@@ -58,6 +58,72 @@ def load_gui():
     get_ipython().run_cell_magic('HTML', '', imports)
 
 
+def log_progress(iterable, every=None, size=None, name='Items'):
+    """
+
+    :param iterable:
+    :param every:
+    :param size:
+    :param name:
+    :return: None
+    """
+
+    from ipywidgets import IntProgress, HTML, VBox
+    from IPython.display import display
+
+    is_iterator = False
+    if size is None:
+        try:
+            size = len(iterable)
+        except TypeError:
+            is_iterator = True
+    if size is not None:
+        if every is None:
+            if size <= 200:
+                every = 1
+            else:
+                every = int(size / 200)  # every 0.5%
+    else:
+        assert every is not None, 'sequence is iterator, set every'
+
+    if is_iterator:
+        progress = IntProgress(min=0, max=1, value=1)
+        progress.bar_style = 'info'
+    else:
+        progress = IntProgress(min=0, max=size, value=0)
+    label = HTML()
+    box = VBox(children=[label, progress])
+    display(box)
+
+    index = 0
+    try:
+        for index, record in enumerate(iterable, 1):
+            if index == 1 or index % every == 0:
+                if is_iterator:
+                    label.value = '{name}: {index} / ?'.format(
+                        name=name,
+                        index=index
+                    )
+                else:
+                    progress.value = index
+                    label.value = u'{name}: {index} / {size}'.format(
+                        name=name,
+                        index=index,
+                        size=size
+                    )
+            yield record
+    except:
+        progress.bar_style = 'danger'
+        raise
+    else:
+        progress.bar_style = 'success'
+        progress.value = index
+        label.value = "{name}: {index}".format(
+            name=name,
+            index=str(index or '?')
+        )
+
+
 # ======================================================================================================================
 # Register
 # ======================================================================================================================
