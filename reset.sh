be648f703b2dd2961d36a096fa99f7b8cc144b24
Clarence Mah <clarence.iyan@gmail.com>
2017-02-22 18:01:52 -0800

    added mac install

--- reset.sh
+++ reset.sh
@@ -2,7 +2,8 @@
 pip uninstall -y simpli ipywidgets jupyter_declarativewidgets
 sudo npm uninstall -g bower
 sudo apt remove -y npm
+brew remove npm
 
 # Install
-python setup.py install
-jupyter notebook ~ --no-browser
+# python setup.py install
+# jupyter notebook ~ --no-browser
