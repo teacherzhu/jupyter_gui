acb82f38393978589eb4357e3ad554310bc239dd
KwatME <kwat.medetgul.ernar@gmail.com>
2017-02-22 16:32:54 -0800

    setup.py

--- setup.py
+++ setup.py
@@ -8,8 +8,24 @@ class InstallCommand(install):
     def run(self):
         install.run(self)
 
+        cmd = ''
+
+        if 'linux' in platform:
+            cmd += '''
+            sudo apt install -y npm
+            sudo ln -s /usr/bin/nodejs /usr/bin/node
+            '''
+        elif 'darwin' in platform:
+            pass
+        elif 'win' in platform:
+            pass
+
+        cmd += '''
+        npm install -g bower
+        '''
+
         # comment out bower installs when running as dev
-        cmd = """
+        cmd += '''
         jupyter nbextensions_configurator enable --user
         jupyter nbextension install --py --user simpli --symlink
         jupyter nbextension enable --py --user simpli
@@ -19,35 +35,23 @@ class InstallCommand(install):
         jupyter nbextension install --py --user declarativewidgets
         jupyter nbextension enable --py --user declarativewidgets
         jupyter serverextension enable --py --user declarativewidgets
-"""
-        # bower install --save PolymerElements/iron-form
-        # bower install --save PolymerElements/paper-input
-        # bower install --save PolymerElements/iron-label
-        # bower install --save PolymerElements/paper-button
-        # bower install --save PolymerElements/iron-icon
-        # bower install --save PolymerElements/paper-material
-        # bower install --save PolymerElements/paper-header-panel
-        # bower install --save PolymerElements/iron-collapse
-        # bower install --save Collaborne/paper-collapse-item
-        # """
 
-        if 'linux' in platform:
-            cmd += """
-            sudo apt install -y npm
-            sudo ln -s /usr/bin/nodejs /usr/bin/node
-            """
-        elif 'darwin' in platform:
-            cmd += """
-            """
-        elif 'win' in platform:
-            cmd += """
-            """
+        bower install --save PolymerElements/iron-form
+        bower install --save PolymerElements/paper-input
+        bower install --save PolymerElements/iron-label
+        bower install --save PolymerElements/paper-button
+        bower install --save PolymerElements/iron-icon
+        bower install --save PolymerElements/paper-material
+        bower install --save PolymerElements/paper-header-panel
+        bower install --save PolymerElements/iron-collapse
+        bower install --save Collaborne/paper-collapse-item
+        '''
 
         print('Running installation commands ...\n{}'.format(cmd))
         try:
             run(cmd, shell=True)
         except:
-            print(cmd)
+            print('Installation commands error:\n{}'.format(cmd))
 
 
 setup(name='simpli',
