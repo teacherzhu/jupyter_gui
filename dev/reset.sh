eec30324808e49e8ca0f0a867864ffc2f225047d
KwatME <kwat.medetgul.ernar@gmail.com>
2017-02-22 20:01:16 -0800

    Commented README

--- dev/reset.sh
+++ dev/reset.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
-
-pip uninstall simpli -y
-
+# Uninstall
+pip uninstall -y simpli ipywidgets jupyter_declarativewidgets
 jupyter nbextension uninstall --py --sys-prefix simpli
 jupyter nbextension disable --py --sys-prefix simpli
 jupyter serverextension disable --py --sys-prefix simpli
 
 rm -rf ~/.jupyter/
+sudo npm uninstall -g bower
+sudo apt-get remove -y npm
+brew remove npm
--- reset.sh
+++
@@ -1,5 +0,0 @@
-# Uninstall
-pip uninstall -y simpli ipywidgets jupyter_declarativewidgets
-sudo npm uninstall -g bower
-sudo apt-get remove -y npm
-brew remove npm
