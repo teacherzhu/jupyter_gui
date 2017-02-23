f2ce3a427438c810d6f4133f1177ea3ec4930752
KwatME <kwat.medetgul.ernar@gmail.com>
2017-02-22 18:14:54 -0800

    Removed reinstallign form resdt.sh

--- reset.sh
+++ reset.sh
@@ -1,8 +1,4 @@
 # Uninstall
 pip uninstall -y simpli ipywidgets jupyter_declarativewidgets
 sudo npm uninstall -g bower
-sudo apt remove -y npm
-
-# Install
-python setup.py install
-jupyter notebook ~ --no-browser
+sudo apt-get remove -y npm
