e70d3207945eb5d0f9560eb1c69c1e53cbe47a66
Kate ME <kate.medetgul.ernar@gmail.com>
2017-02-24 13:12:18 -0800

    Renamed example to demo to see if I can push

---
+++ demo_notebook_package/tools/a_library.py
@@ -0,0 +1,15 @@
+def a_function_in_a_library(req_arg1, req_arg2, opt_arg1=None, opt_arg2=None):
+    """
+    A function in a library.
+    :param req_arg1: object; required argument 1.
+    :param req_arg2: object; required argument 2.
+    :param opt_arg1: object; optional argument 1.
+    :param opt_arg2: object; optional argument 2.
+    :return: None
+    """
+
+    print('Required argument 1 & 2: {} & {}.'.format(req_arg1, req_arg2))
+    if opt_arg1:
+        print('Optional argument 1: {}.'.format(opt_arg1))
+    if opt_arg2:
+        print('Optional argument 2: {}.'.format(opt_arg2))
